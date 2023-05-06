1818a44e7f55518b7281004c519608b0f9934b6f
CemYilmaz <cemyilmaz463@gmail.com>
2023-05-06 14:56:21 +0300

    constant item price changed

--- lib/constants/item_consts.dart
+++ lib/constants/item_consts.dart
@@ -5,7 +5,7 @@ List<Product> productList = [
     id: 'p1',
     name: 'Red Shirt',
     description: 'A red shirt - it is pretty red!',
-    price: 29.99,
+    price: 30.00,
     imageUrl:
     'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_960_720.jpg',
   ),
@@ -13,7 +13,7 @@ List<Product> productList = [
     id: 'p2',
     name: 'Trousers',
     description: 'A nice pair of trousers.',
-    price: 59.99,
+    price: 60.00,
     imageUrl:
     'https://images.unsplash.com/photo-1581291512481-9e9b6d1b9b8e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHJvdXNlcnN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
   ),
@@ -21,7 +21,7 @@ List<Product> productList = [
     id: 'p3',
     name: 'Yellow Scarf',
     description: 'Warm and cozy - exactly what you need for the winter.',
-    price: 19.99,
+    price: 20.00,
     imageUrl:
     'https://images.unsplash.com/photo-1581291512481-9e9b6d1b9b8e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHJvdXNlcnN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
   ),
