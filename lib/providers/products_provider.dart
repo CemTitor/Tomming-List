aacf614f9668b212c04f8e2039c042cdb922c3f3
CemYilmaz <cemyilmaz463@gmail.com>
2023-05-06 18:02:47 +0300

    searchProducts and filterProducts methods added

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -13,4 +13,27 @@ class ProductsProvider with ChangeNotifier {
     return _products.firstWhere((product) => product.id == id);
   }
 
+  List<Product> searchProducts(String query) {
+    if (query.isEmpty) {
+      return products;
+    } else {
+      return _products
+          .where((product) =>
+          product.name.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    }
+  }
+
+  List<Product> filterProducts(FilterOptions filterOption) {
+    List<Product> filteredProducts = products;
+    switch (filterOption) {
+      case FilterOptions.LowestPrice:
+        filteredProducts.sort((a, b) => a.price.compareTo(b.price));
+        break;
+      case FilterOptions.HighestPrice:
+        filteredProducts.sort((a, b) => b.price.compareTo(a.price));
+        break;
+    }
+    return filteredProducts;
+  }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,19 +5,76 @@ import 'package:shopping_cart_tom/widgets/product_item.dart';
 
 import '../providers/theme_provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String _searchQuery = '';
 
   @override
   Widget build(BuildContext context) {
     final productsProvider = Provider.of<ProductsProvider>(context);
     final themeProvider = Provider.of<ThemeProvider>(context);
-    final products = productsProvider.products;
+    var products = productsProvider.searchProducts(_searchQuery);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('E- Commerce TOM'),
+        title: TextField(
+          onChanged: (value) {
+            setState(() {
+              _searchQuery = value;
+            });
+          },
+          decoration: InputDecoration(
+            hintText: 'Search...',
+            border: InputBorder.none,
+          ),
+        ),
         actions: [
+          IconButton(
+            icon: Icon(Icons.filter_list),
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (ctx) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 200,
+                      child: Column(
+                        children: [
+                          ListTile(
+                            title: const Text('Lowest Price'),
+                            onTap: () {
+                              setState(() {
+                                products = productsProvider.filterProducts(FilterOptions.LowestPrice);
+                              });
+                              Navigator.of(ctx).pop();
+                            },
+
+                          ),
+                          ListTile(
+                            title: const Text('Highest Price'),
+                            onTap: () {
+                              setState(() {
+                                products = productsProvider.filterProducts(FilterOptions.HighestPrice);
+                              });
+                              Navigator.of(ctx).pop();
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+
           Row(
             children: [
               Text('Dark Mode'),
@@ -46,4 +103,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
