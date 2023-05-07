1a78352ff0fa361be83d8e116ba4c5e3e8c70681
CemYilmaz <cemyilmaz463@gmail.com>
2023-05-07 14:13:19 +0300

    Animation and adding, removing functions added to Product Item on the Home Screen

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,62 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shopping_cart_tom/models/product.dart';
-import 'package:shopping_cart_tom/screens/product_details_screen.dart';
+import 'package:shopping_cart_tom/providers/cart_provider.dart';
 
-class ProductItem extends StatelessWidget {
+import '../screens/product_details_screen.dart';
+
+class ProductItem extends StatefulWidget {
   final Product product;
 
-  const ProductItem(this.product, {super.key});
+  ProductItem(this.product);
+
+  @override
+  _ProductItemState createState() => _ProductItemState();
+}
+
+class _ProductItemState extends State<ProductItem> {
+  int _quantity = 0;
+
+  void _incrementQuantity() {
+    setState(() {
+      _quantity++;
+    });
+  }
+
+  void _decrementQuantity() {
+    if (_quantity > 0) {
+      setState(() {
+        _quantity--;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    final cartProvider = Provider.of<CartProvider>(context, listen: false);
+
+    return GestureDetector(
       onTap: () {
-        Navigator.pushNamed(
-          context,
-          ProductDetailsScreen.routeName,
-          arguments: product.id,
-        );
+        Navigator.pushNamed(context, ProductDetailsScreen.routeName,
+            arguments: widget.product.id);
       },
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        elevation: 4,
-        margin: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(10),
-              ),
-              child: Image.network(
-                product.imageUrl!,
-                height: 200,
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        decoration: BoxDecoration(
+          color: _quantity > 0 ?  Theme.of(context).primaryColor : Theme.of(context).cardColor,
+          borderRadius: BorderRadius.circular(5),
+          boxShadow: [
+            BoxShadow(
+              color: _quantity > 0 ? Theme.of(context).primaryColor : Theme.of(context).shadowColor,
+              spreadRadius: 1,
+              blurRadius: 4,
+              offset: Offset(0, 2),
             ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
+          ],
+        ),
+        child: Card(
+          elevation: 0,
+          child: Stack(
+            children: [
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    product.name,
-                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                children: [
+                  Image(
+                    image: AssetImage(widget.product.imageUrl!),
+                    width: double.infinity,
                   ),
-                  const SizedBox(height: 5),
-                  Text(
-                    '\$${product.price.toStringAsFixed(2)}',
-                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      widget.product.name,
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      '\$${widget.product.price}',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ],
               ),
-            ),
-          ],
+              Positioned(
+                top: 0,
+                right: 0,
+                child: _quantity == 0
+                    ? IconButton(
+                  icon: Icon(Icons.add, color: Colors.black),
+                  onPressed: () {
+                    _incrementQuantity();
+                    cartProvider.addItem(widget.product, 1);
+                  },
+                )
+                    : Container(
+                  color: Colors.black54,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.add, color: Colors.white),
+                        onPressed: () {
+                          _incrementQuantity();
+                          cartProvider.addItem(widget.product, 1);
+                        },
+                      ),
+                      Text(
+                        '$_quantity',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.remove, color: Colors.white),
+                        onPressed: () {
+                          _decrementQuantity();
+                          cartProvider.removeItem(widget.product.id);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
