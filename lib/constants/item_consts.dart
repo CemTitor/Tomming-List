0ab0cd38797b0494cdd409b69f4f5606e861b956
CemYilmaz <cemyilmaz463@gmail.com>
2023-05-07 14:10:16 +0300

    Fruit icons pngs added

--- lib/constants/item_consts.dart
+++ lib/constants/item_consts.dart
@@ -3,26 +3,26 @@ import 'package:shopping_cart_tom/models/product.dart';
 List<Product> productList = [
   Product(
     id: 'p1',
-    name: 'Red Shirt',
+    name: 'Apple',
     description: 'A red shirt - it is pretty red!',
     price: 30.00,
     imageUrl:
-    'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_960_720.jpg',
+    'assets/icons/food/icons8-apple-100.png',
   ),
   Product(
     id: 'p2',
-    name: 'Trousers',
+    name: 'Blueberry',
     description: 'A nice pair of trousers.',
     price: 60.00,
     imageUrl:
-    'https://images.unsplash.com/photo-1581291512481-9e9b6d1b9b8e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHJvdXNlcnN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
+    'assets/icons/food/icons8-blueberry-100.png',
   ),
   Product(
     id: 'p3',
-    name: 'Yellow Scarf',
+    name: 'Cherry',
     description: 'Warm and cozy - exactly what you need for the winter.',
     price: 20.00,
     imageUrl:
-    'https://images.unsplash.com/photo-1581291512481-9e9b6d1b9b8e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHJvdXNlcnN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
+    'assets/icons/food/icons8-cherry-100.png',
   ),
 ];
